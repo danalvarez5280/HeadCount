af45c7781e5380c68fe8baee542c771c7e633a59
Dan Alvarez <danalvarez5280@gmail.com>
2017-08-14 17:22:24 -0600

    got the first set of tests to pass and set up our helper class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import DistrictRepository from './helper';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Welcome To Headcount 2.0</div>
+      <div>Welcome To Headcount 2.0
+      <DistrictRepository />
+      </div>
+      
     );
   }
 }
