f9322dbc88b9dfcbb346a92b33a8b99c7c639bec
Ana <analujauregui@gmail.com>
2017-08-15 15:50:12 -0600

    Helper function, passing tests iteration 1-3

--- src/helper.js
+++ src/helper.js
@@ -1,32 +1,35 @@
-// import kinderData from '../data/kindergartners_in_full_day_program';
-
-// export default class DistrictRepository {
-//   constructor(kinderData) {
-//     this.data = kinderData.reduce((acc, elem) => {
-//       if(!acc[elem.Location]) {
-//         acc[elem.Location] = []
-//       }
-//       acc[elem.Location].push(elem)
-//       
-//       return acc
-//     }, {})
-//   }
-// }
-
 export default class DistrictRepository {
   constructor(rawData){
     this.data = this.newData(rawData)
   }
-  
+
   newData(data) {
     return data.reduce((acc, elem) => {
       if(!acc[elem.Location]) {
-        acc[elem.Location] = []
-        }
-      acc[elem.Location].push(elem)
-          
+        acc[elem.Location] = {location: elem.Location.toUpperCase(), data: {}}
+      }
+        acc[elem.Location].data[elem.TimeFrame] = Math.round(1000 * elem.Data) / 1000 || 0;
+
       return acc
     }, {})
-    
   }
+
+  findByName(name) {
+    if (name) {
+      let keys = Object.keys(this.data);
+      let found = keys.find(location => location.toUpperCase() === name.toUpperCase())
+      return this.data[found]
+    }
+  }
+
+  findAllMatches(districtName) {
+    let keys = Object.keys(this.data)
+    let matches = keys.map(location => this.data[location])
+
+    if (districtName) {
+      return matches.filter(locationName => locationName.location.includes(districtName.toUpperCase()))
+    }
+    return matches;
+  }
+
 }
