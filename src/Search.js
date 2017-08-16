51d24a9af4aed4ec82391f18c8a02f4c34f26b64
Ana <analujauregui@gmail.com>
2017-08-16 11:01:11 -0600

    Add search functionality

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
-import DistrictRepository from './helper';
 
 export default class Search extends Component {
   constructor() {
     super();
 
-    this.state = {
-      districtSearch: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({
-      districtSearch: e.target.value
-    })
   }
 
   handleClick() {
@@ -26,8 +14,8 @@ export default class Search extends Component {
   render() {
     return(
       <div className='search-field'>
-        <input className='search' placeholder='Search' value={this.state.districtSearch} onChange={this.handleChange}/>
-        <button className='submit-btn' type='Submit' onClick={this.handleClick}> Submit </button>
+        <input className='search' placeholder='Search' onChange={ this.props.handleChange }/>
+        <button className='submit-btn' type='Submit' onClick={ this.handleClick }> Submit </button>
       </div>
     )
   }
